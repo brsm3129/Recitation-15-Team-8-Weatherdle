4f769e75b78aff6ac5acffe8b8f60a3ee93c53c9
Adam Adl <adad8283@colorado.edu>
2023-04-13 12:50:30 -0600

    Adding code for Lab 11

--- Source Code/src/index.js
+++ Source Code/src/index.js
@@ -27,25 +27,72 @@ const dbConfig = {
 // Connect to database using the above details
 const db = pgp(dbConfig);
 
+// test your database
+db.connect()
+  .then(obj => {
+    console.log('Database connection successful'); // you can view this message in the docker compose logs
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+    console.log('ERROR:', error.message || error);
+  });
+
+const user = {
+    username: undefined,
+    password: undefined,
+    first_name: undefined,
+    last_name: undefined,
+    email: undefined,
+  };
+
+
 // ****************************************************
 // <!-- Section 3 : Examples Enpoint Implementation-->
 // ****************************************************
 
 app.set('view engine', 'ejs'); // set the view engine to EJS
-// <!-- Endpoint 1 :  Default endpoint ("/") -->
-const message = 'Hey there!';
-app.get('/', (req, res) => {
-  res.send(message);
+
+app.get('/welcome', (req, res) => {
+  res.json({status: 'success', message: 'Welcome!'});
 });
 
 // ************************************************
 // <!-- Section 4 : TODOs Enpoint Implementation-->
 // ************************************************
 
+// TODO - Include your API routes here
+app.get('/', (req, res) => {
+  res.redirect('/login'); //this will call the /anotherRoute route in the API
+});
+
+app.get('/login', (req,res) => {
+  res.render('pages/login');
+})
+
+app.post('/login', async(req,res)=>{
+  // check if password from request matches with password in DB
+  db.query("SELECT password FROM users WHERE username = ($1);", [req.body.username])
+  .then(async query => {
+    const passwordMatch = await bcrypt.compare(req.body.password, query[0].password);
+    user.username = req.body.username;
+    if(!user || !passwordMatch){
+      res.render('pages/login', {message: "Incorrect username or password", error:true});
+    }
+    else{
+      req.session.user = user;
+      req.session.save();
+      res.redirect('/discover');
+    }
+  })
+  .catch(error => {
+    res.redirect('/register');
+  });
+});
+
 // *********************************
 // <!-- Section 5 : Start Server-->
 // *********************************
 // starting the server and keeping the connection open to listen for more requests
-app.listen(3000, () => {
+module.exports = app.listen(3000, () => {
   console.log('listening on port 3000');
 });
