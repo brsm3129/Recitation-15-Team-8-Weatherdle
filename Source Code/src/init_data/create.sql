940b949b720ed33741bdf209c99efa445a58987b
Aryan Singh <arsi6998@colorado.edu>
2023-04-25 04:04:12 -0600

    Data base and Fetching data from api succefully

--- Source Code/src/init_data/create.sql
+++ Source Code/src/init_data/create.sql
@@ -3,3 +3,14 @@ CREATE TABLE users(
     username VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL
 );
+
+DROP TABLE IF EXISTS weather_data CASCADE;
+CREATE TABLE weather_data(
+    city VARCHAR(50),
+    sta CHAR(60),
+    dat VARCHAR(50),
+    high_temp  FLOAT,
+    low_temp FLOAT, 
+    sunrise VARCHAR(50), 
+    sunset VARCHAR(50)
+);
