f0804f5ee4e1fbb1124d470d7c272322f5ebee62
Adam Adl <adad8283@colorado.edu>
2023-04-27 17:25:57 -0600

    Attempted guess table query

--- Source Code/src/init_data/create.sql
+++ Source Code/src/init_data/create.sql
@@ -27,3 +27,15 @@ CREATE TABLE userdata(
     );
 INSERT INTO userdata (username, pfp, streak, avgGuess, totalGames, totalGuesses, correctGuesses) VALUES ('test0', 1, 3, 5, 4, 20, 4);
 
+DROP TABLE IF EXISTS guesses CASCADE;
+CREATE TABLE guesses(
+    username VARCHAR(50) PRIMARY KEY,
+    Guess1 VARCHAR(50),
+    Guess2 VARCHAR(50),
+    Guess3 VARCHAR(50),
+    Guess4 VARCHAR(50),
+    Guess5 VARCHAR(50),
+    Guess6 VARCHAR(50),
+    Guess7 VARCHAR(50),
+    Guess8 VARCHAR(50)
+);
