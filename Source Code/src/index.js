8e31127a85d795c857fba6811d652d8f0a1c22b0
Peyton Barger <peba1207@colorado.edu>
2023-05-01 18:44:05 -0600

    working

--- Source Code/src/index.js
+++ Source Code/src/index.js
@@ -96,23 +96,29 @@ app.get('/register', (req, res) => {
   res.render('pages/register');
 });
 app.get('/profile', (req, res) => {
-  var username = req.query.username;
-  //Userdata query
-  var query = `select * from userdata WHERE username = '${username}';`
-  //User ranking query
-  var query2 = `select t.Rank from (select ROW_NUMBER() OVER(ORDER BY streak DESC) AS Rank, username FROM userdata) t where t.username = '${username}'`;
-  var query3 = `select t.Rank from (select ROW_NUMBER() OVER(ORDER BY avgGuess ASC) AS Rank, username FROM userdata) t where t.username = '${username}'`;
-
-  if(!username){
-    console.log("COOLIO");
-    if (!req.session.user.username) {
-      res.redirect('pages/weatherdle');
+  var username = null;
+  var isUser = false;
+  if(!req.query.username){
+    if (!req.session.user) {
+      res.redirect('/weatherdle');
     } else {
       username = req.session.user.username;
+      isUser = true;
     }
+  } else{
+    username = req.query.username
   }
   console.log(username);
-  //console.log(query);
+  console.log(query);
+
+    //Userdata query
+    var query = `select * from userdata WHERE username = '${username}';`
+    //User ranking query
+    var query2 = `select t.Rank from (select ROW_NUMBER() OVER(ORDER BY streak DESC) AS Rank, username FROM userdata) t where t.username = '${username}'`;
+    var query3 = `select t.Rank from (select ROW_NUMBER() OVER(ORDER BY avgGuess ASC) AS Rank, username FROM userdata) t where t.username = '${username}'`;
+  
+
+  if(username){
   db.task('get-everything', task => {
     return task.batch([task.any(query), task.any(query2), task.any(query3)]);
   })
@@ -123,6 +129,7 @@ app.get('/profile', (req, res) => {
       res.render('pages/profile', {
         user: data[0][0],
         rank: [data[1][0].rank, data[2][0].rank],
+        isUser
       });
     })
     // if query execution fails
@@ -136,8 +143,41 @@ app.get('/profile', (req, res) => {
         error: err,
       });
     });
-  
+  }
 
+});
+app.get('/profile/change', (req,res) =>{
+  res.redirect('/profile');
+});
+app.post('/profile/change', (req,res) => {
+  var username = null;
+
+  if(!req.query.username){
+    if (!req.session.user) {
+      res.redirect('/weatherdle');
+    } else {
+      username = req.session.user.username;
+    }
+  } else{
+    username = req.query.username
+  }
+
+  var query = `UPDATE userdata SET pfp = $1 WHERE username = $2`;
+
+  db.any(query, [req.body.pfp, username])
+  // if query execution succeeds
+  // send success message
+  .then(function (data) {
+    console.log(query);
+    
+    res.redirect('/profile');
+    
+  })
+  // if query execution fails
+  // send error message
+  .catch(function (err) {
+    return console.log(err);
+  });
 });
 
 app.get('/leaderboard', (req, res) => {
